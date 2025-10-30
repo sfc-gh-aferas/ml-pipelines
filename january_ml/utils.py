ac75503562f81de3f1e56b6be18a8971529b27cf
Allie Feras <allie.feras@snowflake.com>
2025-10-30 15:34:47 -0400

    add feature store and datasets with versioning to template notebook for discussion

--- january_ml/utils.py
+++ january_ml/utils.py
@@ -1,5 +1,9 @@
 import yaml
 import os
+import json
+import hashlib
+from snowflake.ml.feature_store import FeatureView
+from snowflake.snowpark import DataFrame
 
 def load_config(project_name: str) -> dict:
     if "config.yml" in os.listdir(os.getcwd()):
@@ -15,3 +19,22 @@ def load_config(project_name: str) -> dict:
     if config["project_name"] == project_name:
         return config
     raise ValueError(f"Config file at {config_dir} is for project {config['project_name']}, not {project_name}")
+
+def version_featureview(feature_view:FeatureView) -> str:
+
+    """Computes an md5 hash of a feature view's query and the key/value of metadata fields.
+    This hash can be used as the version when registering the feature view."""
+
+    keys = [
+        'query', 'name','entities','timestamp_col','desc','feature_desc',
+        'refresh_freq','database','schema','initialize','warehouse',
+        'refresh_mode','refresh_mode_reason','owner','cluster_by'
+    ]
+    data = {k:str(getattr(feature_view, "_"+k)) for k in keys}
+    return hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest().upper()
+
+def version_data(df:DataFrame) -> str:
+
+    """Computes an md5 hash on the data itself to be used as a dataset version."""
+
+    return hashlib.md5(str(df.select_expr("HASH_AGG(*)")).encode('utf-8')).hexdigest().upper()
