15cea689259dba116909a921e9f40f87be431458
Allie Feras <allie.feras@snowflake.com>
2026-03-18 10:41:33 -0400

    use uv for package management

--- feature_store/feature_views.py
+++ feature_store/feature_views.py
@@ -4,7 +4,6 @@ import snowflake.snowpark.functions as F
 def create_example_features(session: Session) -> DataFrame:
 
     df = session.table('MORTGAGE_LENDING_DEMO_DATA')
-
     df = df.with_column("HOUR", F.date_part("HOUR","CREATED_AT"))
     df = df.with_column("OPENED", F.col("OPEN_AT").is_not_null())
     df = df.select("BORROWERCOMM_UUID","CREATED_AT","HOUR","OPENED")
