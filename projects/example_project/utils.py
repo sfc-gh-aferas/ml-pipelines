cb34e1761a18276fa1f6b956a0c07de322ba3496
Allie Feras <allie.feras@snowflake.com>
2026-03-19 13:31:12 -0400

    python version

--- projects/example_project/utils.py
+++ projects/example_project/utils.py
@@ -11,6 +11,7 @@ def install_stage_package():
     except ValueError:
         env = "DEV"
     os.environ["SNOWFLAKE_ENVIRONMENT"] = env
+    
     fq_schema = f"ML_PIPELINE_{env}_DB.ML_PROJECTS"
     session.file.get(f"@{fq_schema}.BUILD_STAGE/example_project/dist/ml_utils-0.0.1-py3-none-any.whl", "/tmp")
     subprocess.call([sys.executable, '-m', 'pip', 'install', "/tmp/ml_utils-0.0.1-py3-none-any.whl"])
